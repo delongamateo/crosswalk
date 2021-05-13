c72c91cde66de618202d507c75894e920ff9e622
delongamateo <delongamateo@gmail.com>
2021-05-13 20:08:49 +0200

    DOM practice

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// U varijable spremamo divove sa slikama i button
+// Saving div's and button in variable's
 let stopOn = document.querySelector(".stop-on");
 let stopOff = document.querySelector(".stop-off");
 let button = document.querySelector(".switch");
 
-//funkcija koja ce kad se pozove sakrit trenutno peikazan div i pokazat skriveni
+//Function that will hide one div and show another
 const youCanWalk = () => {
 
    stopOn.style.display = "none";
    stopOff.style.display = "flex";
 
-    // callback funkcija koja ce nakon pet sekundi vratit prvi div a sakrit drugi
+    // callback function that will show first div and hide second after 5 sec
     setTimeout(() => {
    stopOn.style.display = "flex";
    stopOff.style.display = "none";
 }, 5000)
 }
-// dodajemo event listener na button kad se klikne pozove funkciju youCanWalk
+// add event listener that will on click on button call youCanWalk function
 button.addEventListener("click", youCanWalk)
