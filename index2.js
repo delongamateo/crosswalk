7544cd6a2cbae6d324a7ec316477269ee8389d12
delongamateo <delongamateo@gmail.com>
2021-05-16 22:17:24 +0200

    added countdown time

--- index2.js
+++ index2.js
@@ -7,17 +7,76 @@ let button = document.querySelector(".switch");
 const youCanWalk = () => {
 
    stopOn.style.display = "none";
-   stopOff.style.display = "flex";
+   stopOff.style.display = "block";
 
     // callback function that will show first div and hide second after 5 sec
     setTimeout(() => {
-   stopOn.style.display = "flex";
-   stopOff.style.display = "none";
-}, 5000)
+      stopOn.style.display = "block";
+      stopOff.style.display = "none";
+  }, 5000)
 }
+
 // add event listener that will on click on button call youCanWalk function but it will wait 3 sec
 button.addEventListener("click", () => {
     setTimeout(youCanWalk, 3000)
-})
+});
+
+// adding countdown
+
+let count = document.querySelector(".countdown");
+
+
+
+const countDown = () => {
+  let numbers = 3;
+
+
+  
+
+  const count1 = setInterval(() => {
+    numbers = numbers - 1;
+
+    count.innerHTML = numbers;
+
+    if (numbers === 0) {
+      clearInterval(count1);
+    }
+
+  }, 1000)
+
+
+
+};
+
+button.addEventListener("click", countDown);
+
+
+const timeLeft = () => {
+  let numbers = 5;
+
+  const count1 = setInterval(() => {
+    numbers = numbers - 1;
+
+    count.innerHTML = numbers;
+
+    if (numbers === 0) {
+      clearInterval(count1);
+    }
+
+  }, 1000);
+
+
+};
+
+button.addEventListener("click", () => {
+  setTimeout(timeLeft, 3000);
+});
+
+
+
+
+
+  
+
 
 
